e7825281223d61ac4e3f88fa8e0fad3122e34a2a
Willfredy742 <wilfredy742@gmail.com>
2026-07-28 19:00:53 +0200

    feat/img

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import Saludo from "./components/Saludo";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Testimony from "./components/Testimony";
+
 
 
 function App(){
   return(
     <>
         <Header />
-        <h1>Mi primera app en react 👋</h1>
-        <Saludo />
+        
+        <Testimony
+        	name='Emma Bostian'
+          country='Suecia'
+          images='emma'
+          post='Ingeniera de Software'
+          company='Spotify'
+          testimony='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
+        />
+        
         <Footer />
     </>
     
---
+++ src/components/Testimony.jsx
@@ -0,0 +1,42 @@
+import '../styles/Testimony.css';
+
+
+const images = import.meta.glob('../assets/img/*.png', {
+  eager: true,
+  import: 'default'
+});
+
+function Testimony(props) {
+  return (
+    <div className='containerTestimony'>
+      <img
+        className='imageTestimony'
+        
+        src={images[`../assets/img/${props.images}.png`]}
+        
+        alt={`Photo of ${props.name}`}
+      />
+
+      <div className='testimonialTextContainer'>
+        <p className='testimonialName'>
+          
+          <strong>{props.name}</strong> en {props.country}
+        </p>
+
+        <p className='testimonialPosition'>
+        
+          {props.post} en <strong>{props.company}</strong>
+        </p>
+
+        <p className='testimonialText'>
+        
+          "{props.testimony}"
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimony;
+
+
