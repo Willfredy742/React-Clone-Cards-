18ba861501e84d87a03794c26c967f1882f2e10a
Willfredy742 <wilfredy742@gmail.com>
2026-07-28 16:27:30 +0200

    feat/proof

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Saludo from "./components/Saludo";
+
+function App() {
+  return (
+    <>
+
+    <h1>Mi primera app en react 👋</h1>
+    <saludo />
+    </>
+    
+  )
+}
+
+export default App;
+
